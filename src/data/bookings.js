4f3ac7413b1f1a206e31a5785b8e8dfa5875704f
AmineTssouli <91386414+AmineTssouli@users.noreply.github.com>
2022-11-07 13:12:07 +0200

    fixing

--- src/data/bookings.js
+++ src/data/bookings.js
@@ -4,14 +4,21 @@ export const getBookings =  async (room) => {
     const response = await axios.get(`/reservations?room=${room}`);
     // const response = await axios.get(`https://tilat.lab.fi/getReservations.php?room=${room}`);
     const bookings = response.data;
-    let happening;
-    let test = '';
+    let happening=0;
+    let test = 0;
     bookings.map((booking) => {
         booking.start = new Date(booking.start)
         booking.end = new Date(booking.end)
         let now = new Date();
-        (booking.start <= now && booking.end > now)? happening = 1:happening = 0;
-        test = 'Now is '+now+'\nStart is '+booking.start +'\nEnd is '+booking.end;
+        if(test === 0) {
+          if (booking.start <= now && booking.end > now){
+            happening = 1
+            test=1;
+          }
+
+        }
+        
+      
         return booking;
       })
       
